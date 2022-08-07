57f01747366eced772c4e850ef88275e289d2f85
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2022-08-06 18:03:10 -0700

    Update slackBot.py
    Pivoted from a timer reminding feature to a weather reporting bot

--- slackBot.py
+++ slackBot.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+import datetime as dt
+import requests
 
 
 logging.basicConfig(level=logging.INFO)
@@ -10,12 +12,54 @@ load_dotenv()
 
 SLACK_BOT_TOKEN = os.environ["OAUTH_TOKEN"]
 OAUTH_BOT_TOKEN = os.environ["SLACK_TOKEN"]
+API_KEY = os.environ["WEATHAPI_KEY"]
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 app = App(token=SLACK_BOT_TOKEN)
 
+def getCityWeatherData(city):
+    try:
+        url = BASE_URL + "appid=" + API_KEY + "&q=" + city
+        response = requests.get(url).json()
+        tempK = response['main']['temp']
+        tempC, tempF = kelvin_to_celsius_fahrenheit(tempK)
+        humidity = response['main']['humidity']
+        description = response['weather'][0]['description']
+        sunriseTime = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+        sunsetTime = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+        weatherAtLoc = f"Temperature in {city}: {tempC:.2f} degrees Celcius or {tempF:.2f} degrees Fahrenheit\nHumidity in {city}: {humidity}% \nGeneral Weather in {city}: {description}\nSunrise at {sunriseTime} in your timezone\nSunset at {sunsetTime} in your timezone"
+    except:
+        weatherAtLoc = "Something went wrong. Try another city like London."
+    return weatherAtLoc
+
+
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
 @app.event("app_mention")
 def mention_handler(body, context, payload, options, say, event):
-    say("Hello World!")
+    textContent = payload['text'][15:]
+    try:
+        botAction = textContent[:7]
+        print(botAction)
+        if botAction == "weather":
+            try:
+                cityToCheck = textContent[7:]
+                print(cityToCheck)
+                cityWeather = getCityWeatherData(cityToCheck)
+                say(cityWeather)
+            except:
+                say("Please add a city")
+        else:
+            say("No valid command given")
+    except:
+        say("Please mention more in your command")
+
+
+    
 
 
 @app.event("message")
